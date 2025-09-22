1917a2be29e12387738baaec2759a704bb8fb249
지훈한 <hoony@jihunhan-ui-MacBookAir.local>
2025-09-22 11:06:05 +0900

    fix: Firebase Functions 빌드 오류 수정 - functions 폴더 제거 및 .vercelignore 추가

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // Firebase Functions 폴더를 Next.js 빌드에서 제외
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  experimental: {
+    // Firebase Functions 폴더 무시
+    outputFileTracingExcludes: {
+      '*': ['./functions/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
